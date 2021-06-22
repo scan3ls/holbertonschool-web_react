b7c3e9642603413cc3e44f614c5ca2a44508254b
scan3ls <scan3ls@gmail.com>
2021-06-22 17:27:39 -0500

    Add file

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,13 @@
+interface MajorCredits {
+    credits: number;
+    majorBrand: any;
+}
+
+interface MinorCredits {
+    credits: number;
+    minorBrand: any;
+}
+
+export const sumMajorCredits: MajorCredits = (subject1: any, subject2: any) => {
+    
+};
