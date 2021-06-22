e633013b669e7184fa9a54babc8b0e9797b8b482
scan3ls <scan3ls@gmail.com>
2021-06-22 13:51:04 -0500

    Change prop name for Teacher interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    [prop: string]: any;
+    [propName: string]: any;
 }
 
 interface Directors extends Teacher {
