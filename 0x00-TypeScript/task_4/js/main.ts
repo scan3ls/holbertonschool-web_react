d9c5c565c09776a0bd97d78d6490d0826c0fc03c
scan3ls <scan3ls@gmail.com>
2021-06-22 16:48:54 -0500

    Update

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,25 +1,24 @@
-namespace Subjects {
-    export const cpp: Cpp = new Cpp;
-    export const java: Java = new Java;
-    export const react: React = new React;
-    export const teacher: Teacher = {
-        firstName: 'Logan',
-        lastName: 'Scanlon',
-        experienceTeachingC: 10
-    };
 
-    console.log('C++');
-    cpp.setTeacher(teacher);
-    console.log(cpp.getRequirements());
-    console.log(cpp.getAvailableTeacher());
+export const cpp: Subjects.Cpp = new Subjects.Cpp;
+export const java: Subjects.Java = new Subjects.Java;
+export const react: Subjects.React = new Subjects.React;
+export const teacher: Subjects.Teacher = {
+    firstName: 'Logan',
+    lastName: 'Scanlon',
+    experienceTeachingC: 10
+};
 
-    console.log('Java');
-    java.setTeacher(teacher);
-    console.log(java.getRequirements());
-    console.log(java.getAvailableTeacher());
+console.log('C++');
+cpp.setTeacher(teacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-    console.log('React');
-    react.setTeacher(teacher);
-    console.log(react.getRequirements());
-    console.log(react.getAvailableTeacher());
-}
+console.log('Java');
+java.setTeacher(teacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(teacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
