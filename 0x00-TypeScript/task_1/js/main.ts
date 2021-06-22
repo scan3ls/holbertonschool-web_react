c1e5a74ce8f54f78b3dc6155e8444896b3b1ed50
scan3ls <scan3ls@gmail.com>
2021-06-22 13:49:03 -0500

    Add files

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,51 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [prop: string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+let printTeacher: printTeacherFunction; 
+printTeacher = (firstName:string, lastName: string) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface IStudent {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+interface IStudentConstructor {
+    new (firstName: string, lastName: string): IStudent;
+}
+
+class StudentClass implements IStudent{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return "Currently working"
+    }
+
+    displayName() {
+        return this.firstName
+    }
+}
