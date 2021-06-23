0c747e2c5e733b28b4a01b76968269e0704d6a4c
scan3ls <scan3ls@gmail.com>
2021-06-23 17:43:51 -0500

    Add file

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+const $ = require('jquery');
+
+const elements = [
+    $('<p>').text('Holberton Dashboard'),
+    $('<p>').text('Dashboard data for the students'),
+    $('<button>').text('Click here to get started'),
+    $("<p id='count'></p>"),
+    $('<p>').text('Copyright - Holberton School')
+];
+
+$('body').append(...elements);
+
+function updateCounter(count) {
+    count += 1;
+    $('#count').text(count);
+}
+
+$('button').click(() => {
+    const count = parseInt($('#count')['0'].innerText) | 0;
+    updateCounter(count);
+});
