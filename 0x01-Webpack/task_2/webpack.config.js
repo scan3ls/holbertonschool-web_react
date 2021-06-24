8fc0dbc47da6f06aaef9e09337cf71a2b14c7993
scan3ls <scan3ls@gmail.com>
2021-06-24 15:55:56 -0500

    Add loaders

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -6,5 +6,26 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          bypassOnDebug: true, // webpack@1.x
+                          disable: true, // webpack@2.x and newer
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 };
