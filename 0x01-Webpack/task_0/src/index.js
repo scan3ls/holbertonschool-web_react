c8f7bc6cea3194a75de38f02cdde121688a56d88
scan3ls <scan3ls@gmail.com>
2021-06-24 15:01:04 -0500

    Update

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -8,3 +8,5 @@ const paragraphs = [
 ];
 
 $('body').append(...paragraphs);
+
+// $('body').append('<p>Holberton Dashboard</p>'); $('body').append('<p>Dashboard data for the students</p>'); $('body').append('<p>Copyright - Holberton School</p>');
