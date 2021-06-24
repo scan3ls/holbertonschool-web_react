97c4ea9f442a5d632392d648034a71de112d4aab
scan3ls <scan3ls@gmail.com>
2021-06-24 14:52:49 -0500

    Update

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
-    },
+    entry: './js/dashboard_main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
