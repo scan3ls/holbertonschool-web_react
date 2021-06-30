75291f07b85d1c6be0fcac83931d80e73004c7d0
scan3ls <scan3ls@gmail.com>
2021-06-30 16:10:51 -0500

    Add React

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -41,6 +41,16 @@ module.exports = {
               },
             ]
           },
+          {
+            test: /\.m?jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
         ]
     },
     devtool: "inline-source-map",
--- 0x02-react_intro/task_5/dashboard/src/index.js
+++ 0x02-react_intro/task_5/dashboard/src/index.js
@@ -1,6 +1,21 @@
-const $ = require('jquery');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
 
-const header = $('<h1>Hello</h1>');
-const subHeder = $('<h2>World</h2>');
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="root-notifications" id="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-$('body').append(header, subHeder);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
