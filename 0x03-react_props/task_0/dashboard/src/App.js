4becb508c805c21dc0bb43f10f7210876946c303
scan3ls <scan3ls@gmail.com>
2021-07-03 18:23:35 -0500

    Seperate App into Basic Components

--- 0x03-react_props/task_0/dashboard/src/App.js
+++ 0x03-react_props/task_0/dashboard/src/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import './App.css';
-import logo from './Holberton_logo.jpg';
-import {getFooterCopy, getFullYear} from './utils';
+import Header from './Header/Header';
+import Login from './Login/Login';
+import Footer from './Footer/Footer';
+import Notifications from './Notifications';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
       </div>
-
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password"/>
-        <button>OK</button>
-      </div>
-
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
--- 0x03-react_props/task_0/dashboard/src/index.js
+++ 0x03-react_props/task_0/dashboard/src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Notifications from './Notifications';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="root-notifications" id="root-notifications">
-      <Notifications />
-    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
