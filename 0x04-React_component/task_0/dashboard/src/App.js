c102ac4961ce1200d005f838fa05a4a2cf8d85f3
scan3ls <scan3ls@gmail.com>
2021-07-12 14:15:27 -0500

    Convert App to react class

---
+++ 0x04-React_component/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/Header';
+import Login from './Login/Login';
+import Footer from './Footer/Footer';
+import Notifications from './Notifications';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
