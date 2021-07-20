b634b8a2fde1729cfd05fafc6ed2e4001465163c
scan3ls <scan3ls@gmail.com>
2021-07-20 12:15:42 -0500

    Change styling

--- 0x05-React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ 0x05-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -3,27 +3,33 @@ import { StyleSheet, css } from 'aphrodite';
 
 function Login() {
     return (
-        <div className={css(styles.body)}>
+        <div className={css(styles.bruh)}>
             <p>Login to access the full dashboard</p>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password"/>
-            <button>OK</button>
+            <div className={css(styles.small)}>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password"/>
+                </div>
+                <button style={{maxWidth: '2.5rem'}}>OK</button>
+            </div>
         </div>
     );
 }
 
 const styles = StyleSheet.create({
-    body: {
-        borderTop: '4px solid rgb(223, 57, 81)',
-        borderBottom: '4px solid rgb(223, 57, 81)',
-        minHeight: '20rem',
-        padding: '3rem'
-    },
-
     bruh: {
         margin: 0
+    },
+
+    small: {
+        display: 'flex',
+        '@media (max-width: 900px)': {
+            flexDirection: 'column'
+        }
     }
 });
 
