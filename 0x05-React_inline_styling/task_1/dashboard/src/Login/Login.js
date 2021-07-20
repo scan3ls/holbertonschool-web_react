1769a02ecf50c3b79557d2a08adbea94472451f8
scan3ls <scan3ls@gmail.com>
2021-07-19 22:07:42 -0500

    copy task_0/ to task_1/

---
+++ 0x05-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+function Login() {
+    return (
+        <div className={css(styles.body)}>
+            <p>Login to access the full dashboard</p>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password"/>
+            <button>OK</button>
+        </div>
+    );
+}
+
+const styles = StyleSheet.create({
+    body: {
+        borderTop: '4px solid rgb(223, 57, 81)',
+        borderBottom: '4px solid rgb(223, 57, 81)',
+        minHeight: '20rem',
+        padding: '3rem'
+    }
+});
+
+export default Login;
