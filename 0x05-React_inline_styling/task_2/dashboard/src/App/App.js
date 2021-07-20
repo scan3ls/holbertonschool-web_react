17e4181771de7fa94078a7b447f69bffb758732f
scan3ls <scan3ls@gmail.com>
2021-07-20 11:17:47 -0500

    Copy from task_1/

---
+++ 0x05-React_inline_styling/task_2/dashboard/src/App/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import highOrderComponent from '../HOC/WithLogging';
+import {
+  listCourses,
+  listNotifications 
+} from '../utils';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+function Body(props) {
+
+  if (props.isLoggedIn) {
+    return (
+      <div className="CourseBody">
+        <BodySectionWithMarginBottom title="Course list">
+          <CourseList listCourses={listCourses}/>
+          <BodySection title="News from the school">
+            <p>some text</p>
+          </BodySection>
+        </BodySectionWithMarginBottom>
+      </div>
+    );
+  } else {
+    const WithLoggingLogin = highOrderComponent(Login);
+    return (
+      <BodySectionWithMarginBottom title="Log in to continue">
+        <WithLoggingLogin />
+      </BodySectionWithMarginBottom>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleKeypress = this.handleKeypress.bind(this);
+  }
+
+  handleKeypress(event) {
+    const logout = () => {
+      window.alert('Logging you out');
+      this.props.logOut();
+    };
+
+    if (event.code === 'KeyH' && event.ctrlKey) logout();
+    if (event.code === 'KeyZ' && event.ctrlKey) logout();
+  }
+
+  componentDidMount() {
+    window.addEventListener('keypress', this.handleKeypress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keypress', this.handleKeypress);
+  }
+
+  render() {
+    const isLoggedIn = (this.props.isLoggedIn) ? this.props.isLoggedIn : false;
+
+    return (
+      <React.Fragment>
+        <Notifications displayDrawer={true} listNotifications={listNotifications}/>
+        <div className={css(styles.App)}>
+          <Header />
+          <Body isLoggedIn={isLoggedIn}/>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  logOut: PropTypes.func
+}
+
+App.defaultProps = {
+  logOut: () => {}
+}
+
+const styles = StyleSheet.create({
+  App: { padding: "0rem 0.5rem" },
+
+  body: {},
+
+  footer: {}
+});
+
+export default App;
---
+++ 0x05-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class NotificationItem extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {value, html, markAsRead, id} = this.props;
+
+        let type;
+        if (html) {
+            if (typeof html !== "object") throw new Error(`html must be an object got ${typeof html}`);
+            if (html.__html === undefined) throw new Error('html must have a key of "__html"');
+            if (typeof html.__html !== "string") throw new Error('html.__html must be a string');
+        }
+
+        if (value) {
+            if (typeof value !== 'string') throw new Error('value must be a string');
+        }
+
+        if (this.props.type) {
+            type = this.props.type;
+            if (typeof type !== 'string') throw new Error('type must be a string');
+        } else {
+            type = 'default';
+        }
+
+        const color = (type === 'urgent') ? 'rgb(211, 64, 64)': 'rgb(5, 29, 163)';
+
+        return (
+            (html)
+            ? <li style={{color: color}} data-priority={type} onClick={() => {markAsRead(id)}} dangerouslySetInnerHTML={html}></li>
+            : <li style={{color: color}} data-priority={type} onClick={() => {markAsRead(id)}}>{value}</li>
+        );
+    }
+}
+
+export default NotificationItem;
