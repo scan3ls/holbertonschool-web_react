ef82cc99c6a57ff1c3b585cac59f2152fbc75d9f
scan3ls <scan3ls@gmail.com>
2021-07-27 20:27:24 -0500

    Update

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,5 +1,7 @@
-import { Map, mergeDeep } from 'immutable';
+import { Map } from 'immutable';
 
-export function mergeDeeplyElements(page1, page2) {
-  return Map(mergeDeep(page1, page2));
+export default function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  return map1.mergeDeep(map2);
 }
