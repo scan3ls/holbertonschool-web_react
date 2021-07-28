66c6dec92aadb341a534de7f83157f9df694d32c
scan3ls <scan3ls@gmail.com>
2021-07-27 20:33:51 -0500

    Update

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,11 +2,11 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
   const students = Seq(grades)
-  .filter( x => x.score >= 70 )
-  .map(x => {
-    x.firstName = x.firstName[0].toUpperCase() + x.firstName.slice(1);
-    x.lastName = x.lastName[0].toUpperCase() + x.lastName.slice(1);
-    return x;
-  })
+    .filter((x) => x.score >= 70)
+    .map((x) => {
+      x.firstName = x.firstName[0].toUpperCase() + x.firstName.slice(1);
+      x.lastName = x.lastName[0].toUpperCase() + x.lastName.slice(1);
+      return x;
+    });
   console.log(students.toJSON());
 }
