28c4ba4b2d063c662b5a5fbe55241341989d8fbd
scan3ls <scan3ls@gmail.com>
2021-07-27 20:05:40 -0500

    Update

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,6 +1,5 @@
-import { Map } from 'immutable';
+import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const map = Map(object);
-  return map.getIn(array);
+  return getIn(fromJS(object), array);
 }
