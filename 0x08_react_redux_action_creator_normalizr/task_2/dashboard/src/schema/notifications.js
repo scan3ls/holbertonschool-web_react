e88cb62c16d4400f00229e8980559eaedb3a580d
scan3ls <scan3ls@gmail.com>
2021-07-30 12:43:40 -0500

    Filter normalized Schema

--- 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,18 +1,6 @@
 import * as notifications from '../../notifications.json';
 import { normalize, schema } from 'normalizr';
 
-export function getAllNotificationsByUser(userId) {
-    const list = [];
-
-    for (const item in notifications) {
-        const {id, author, context} = notifications[item];
-        if (author === undefined) continue;
-        if (author.id === userId) list.push(context);
-    }
-
-    return list;
-}
-
 const user = new schema.Entity("users");
 const message = new schema.Entity("messages", undefined, {idAttribute: 'guid'});
 const notification = new schema.Entity("notifications",{
@@ -21,4 +9,19 @@ const notification = new schema.Entity("notifications",{
 });
 const normalizeData = normalize(notifications, [notification]);
 
-export { normalizeData };
+function getAllNotificationsByUser(userId) {
+    const list = normalizeData.entities.notifications;
+    const messages = normalizeData.entities.messages;
+
+    const results = [];
+
+    for (const key in list) {
+        const message = list[key];
+        const {author, context} = message;
+        if (author === userId) results.push(messages[context]);
+    }
+
+    return results;
+}
+
+export { normalizeData, getAllNotificationsByUser };
