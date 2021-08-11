40d5fc6626297814efa947f8fe2db178ff86197d
scan3ls <scan3ls@gmail.com>
2021-08-11 13:43:59 -0500

    Add tests

--- 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -4,27 +4,57 @@ import assert from 'assert';
 import {initialState, uiReducer} from './uiReducer';
 
 describe('basic reducer', () => {
-    it('No action', () => {
-        const state = uiReducer(undefined, undefined).toJSON();
-        const recieved = JSON.stringify(state);
-        const expected = JSON.stringify(initialState);
-
-        assert.equal(recieved, expected);
+    describe('Invalid actions', () => { 
+        it('No action', () => {
+            const state = uiReducer(undefined, undefined).toJSON();
+            const {isNotificationDrawerVisible, isUserLoggedIn, user} = state;
+    
+            assert.equal(isNotificationDrawerVisible, false);
+            assert.equal(isUserLoggedIn, false);
+            assert.equal(JSON.stringify(user), JSON.stringify({}));
+        });
+    
+        it('Wrong Type', () => {
+            const state = uiReducer(undefined, invalidAction.selectCourse(1)).toJSON();
+            const {isNotificationDrawerVisible, isUserLoggedIn, user} = state;
+    
+            assert.equal(isNotificationDrawerVisible, false);
+            assert.equal(isUserLoggedIn, false);
+            assert.equal(JSON.stringify(user), JSON.stringify({}));
+        });
     });
 
-    it('Invalid Action', () => {
-        const state = uiReducer(undefined, invalidAction.selectCourse(1)).toJSON();
-        const recieved = JSON.stringify(state);
-        const expected = JSON.stringify(initialState);
+    describe('valid actions', () => {
+        it('displayNotificationDrawer', () => {
+            const state = uiReducer(undefined, validActions.displayNotificationDrawer()).toJSON();
 
-        assert.equal(recieved, expected);
-    });
+            assert.equal(state.isNotificationDrawerVisible, true);
+        });
 
-    it('Valid action', () => {
-        const state = uiReducer(undefined, validActions.displayNotificationDrawer()).toJSON();
-        const recieved = JSON.stringify(state);
-        const expected = JSON.stringify({...initialState, isNotificationDrawerVisible: true});
+        it('hideNotificationDrawer', () => {
+            let state = uiReducer(undefined, validActions.displayNotificationDrawer());
+            state = uiReducer(state, validActions.hideNotificationDrawer()).toJSON();
 
-        assert.equal(recieved, expected);
+            assert.equal(state.isNotificationDrawerVisible, false);
+        });
+
+        it('Login Success', () => {
+            const state = uiReducer(undefined, validActions.loginSuccess()).toJSON();
+
+            assert.equal(state.isUserLoggedIn, true);
+        });
+
+        it('Login Failure', () => {
+            let state = uiReducer(undefined, validActions.loginFailure()).toJSON();
+            assert.equal(state.isUserLoggedIn, false);
+        });
+
+        it('Logout', () => {
+            let state = uiReducer(undefined, validActions.loginSuccess());
+            assert.equal(state.toJSON().isUserLoggedIn, true);
+            state = uiReducer(state, validActions.logout()).toJSON();
+            assert.equal(state.isUserLoggedIn, false);
+        });
+
     });
 });
