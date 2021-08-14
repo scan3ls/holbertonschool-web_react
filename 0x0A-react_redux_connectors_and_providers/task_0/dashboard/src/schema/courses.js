aad7640d632de1f6ce0b3345706448fdc0d08193
scan3ls <scan3ls@gmail.com>
2021-08-14 14:17:48 -0500

    Delete file

--- 0x0A-react_redux_connectors_and_providers/task_0/dashboard/src/schema/courses.js
+++
@@ -1,9 +0,0 @@
-import { normalize, schema } from 'normalizr';
-
-const courses = new schema.Entity('courses');
-
-function coursesNormalizer(data) {
-    return normalize(data, [courses]);
-}
-
-export {coursesNormalizer};
