1932ed164ddb9ca1a427937dfe357bc4cd8f6c6f
scan3ls <scan3ls@gmail.com>
2021-08-20 10:51:46 -0500

    Add reducers to store

--- 0x0A-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -4,24 +4,29 @@ import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import { uiReducer } from './reducers/uiReducer';
+// import { uiReducer } from './reducers/uiReducer';
+import { rootReducer } from './reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const uiStore = configureStore(
-  { reducer: uiReducer }, 
+const {courses, notifications, ui} = rootReducer;
+
+const store = configureStore(
+  { reducer: {courses, notifications, ui} }, 
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 );
 
+console.log("store", store);
+
 ReactDOM.render(
-  <Provider store={uiStore}>
+  <Provider store={store}>
     <App/>
   </Provider>,
   document.getElementById('root')
 );
 
-export {uiStore};
+export {store};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
